7e7c09ef7df4c36da580313546ec6715f2d4060c
Cleber Junior <dwcleb@gmail.com>
2018-09-03 09:16:21 -0300

    ForgotPassword

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,8 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('dwcleb@gmail.com', 'Cleber Jr')
+            .subject('Password Recover')
+        }
+      )
     } catch (err) {
-      return response.status(err.status).send({ error: { message: "Failed Recover Password, verify you e-mail." } })
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Something happened, is impossible to recover your password. Check if you entered a valid email.' } })
     }
   }
 }
